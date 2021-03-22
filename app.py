c1e277e2cd0dcf8d40fe58041d13a6a361c4417b
Ben Linzel <9991873+benlinzel@users.noreply.github.com>
2021-03-22 17:45:32 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from datetime import datetime, timedelta
+app = Flask(__name__)
+
+snoozeUntil = datetime.now()
+print("snooze until: " + str(snoozeUntil))
+
+@app.route('/isSnoozed')
+def hello_world():
+    global snoozeUntil
+    isSnoozed = snoozeUntil > datetime.now()
+    return (str(isSnoozed))
+
+@app.route('/setSnoozeMinutes/<int:addMinutes>')
+def add_minutes(addMinutes):
+    global snoozeUntil
+    snoozeUntil = datetime.now() + timedelta(minutes=addMinutes)
+    return ('snooze until: ' + str(snoozeUntil))
+
+@app.route('/resetSnooze')
+def reset_snooze():
+    global snoozeUntil
+    snoozeUntil = datetime.now()
+    return ('snooze reset')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
